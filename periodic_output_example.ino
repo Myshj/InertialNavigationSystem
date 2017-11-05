2551a7db1fbacf40e6d2dff27bd6fae4ae020c98
Telinna95 <inna.nuitsombre@gmail.com>
2017-11-05 18:37:35 +0200

    Add files via upload

---
+++ periodic_output_example.ino
@@ -0,0 +1,18 @@
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float a = 0.5;
+  float b = 1.5;
+  float c = 2.5;
+  
+  Serial.print(a);
+  Serial.print("\t");
+  Serial.print(b);
+  Serial.print("\t");
+  Serial.println(c);
+
+  delay(100);
+}
